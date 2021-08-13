ffbb1be309d58fd416abdf2db2895cd9d2a8158d
andrepdo <andre.otavio98@gmail.com>
2021-08-13 19:15:17 -0300

    bug horario de ativacao corrigido

--- back-end/src/services/ManageActivationTimeService.ts
+++ back-end/src/services/ManageActivationTimeService.ts
@@ -1,55 +1,47 @@
-import ActivationTime from 'infra/typeorm/entities/ActivationTime';
-import { getRepository, LessThanOrEqual, MoreThanOrEqual } from 'typeorm';
+import { getManager } from 'typeorm';
 import WebSocket from 'ws';
-
-interface ActivateTimeDTO{
-    estufa_id: number;
-    hour_time_on: number;
-    minute_time_on: number;
-    hour_time_off: number;
-    minute_time_off: number;
-}
 
 class ManageActivationTimeService {
     status:string
 
     constructor(){
-        this.status="on"
+        this.status=""
     }
 	public async execute(ws: WebSocket): Promise<void> {
-        const now = new Date()
-        const now_hour = now.getHours()
-        const now_minute = now.getMinutes()
         // console.log(now_hour, now_minute)
-        const sensorDataRepository = getRepository(ActivationTime)
+        const entityManager = getManager();
 
-        const sensorData = await sensorDataRepository.findOne({
-            where:{
-                hour_time_on: LessThanOrEqual(now_hour),
-                minute_time_on: LessThanOrEqual(now_minute),
-                hour_time_off: MoreThanOrEqual(now_hour),
-                minute_time_off: MoreThanOrEqual(now_minute)
-            }
-        });
-        // console.log(sensorData)
+        const activationData = await entityManager.query(`
+            select count(1)
+            from public.activation_time
+            where
+                (case when ((hour_time_off * 60 + minute_time_off) >= (hour_time_on * 60 + minute_time_on))
+                then (case when (extract(hour from now()) * 60 + extract(minute from now())) between (hour_time_on * 60 + minute_time_on) and (hour_time_off * 60 + minute_time_off - 1) then true else false end)
+                else (case when ((extract(hour from now()) * 60 + extract(minute from now())) between (hour_time_on * 60 + minute_time_on) and (hour_time_off * 60 + minute_time_off - 1 + 24*60))
+                or (((extract(hour from now()) * 60 + extract(minute from now())) + 24*60) between (hour_time_on * 60 + minute_time_on) and (hour_time_off * 60 + minute_time_off - 1 + 24*60)) then true else false end)
+                end);
+            `);
+        let pumpStatus = Number(activationData[0]["count"])
 
-        if(!sensorData){
+        if(!pumpStatus){
             const obj_retorno={
                 "type": "pump",
                 "cmd": "off"
             }
-            if(this.status=="on"){
+            if(this.status!="off"){
                 this.status="off"
                 ws.send(JSON.stringify(obj_retorno))
+                console.log(obj_retorno)
             }
         }else{
             const obj_retorno={
                 "type": "pump",
                 "cmd": "on"
             }
-            if(this.status=="off"){
+            if(this.status!="on"){
                 this.status="on"
                 ws.send(JSON.stringify(obj_retorno))
+                console.log(obj_retorno)
             }
         }
         return
